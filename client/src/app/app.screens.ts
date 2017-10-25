59bd402c0000551295b3b537f7c2147c9ee529ec
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 16:37:29 +0300

    [Studio] Add screen "Name"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -2,14 +2,19 @@
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
 import { 111 _PhonePortrait } from '../pages/111/PhonePortrait/111';
+import { Name_PhonePortrait } from '../pages/Name/PhonePortrait/Name';
 export class Screens {
   static declarations: Page[] = [
     MenuComponent,
-    111 _PhonePortrait
+    111 _PhonePortrait,
+    Name_PhonePortrait
   ];
   static mapping = {
     '111': {
       PhonePortrait: 111 _PhonePortrait
+    },
+    'Name': {
+      PhonePortrait: Name_PhonePortrait
     }
   }
 }
