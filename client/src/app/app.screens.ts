2a340c0278ec7a0183ac9671ace65c47b33067af
test <test@test.com>
2017-10-25 16:09:19 +0300

    [Studio] Add screen "One"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,16 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
-import { MenuComponent } from "./menu/menu.component";
-
+import { MenuComponent } from './menu/menu.component';
+import { One_PhonePortrait } from '../pages/One/PhonePortrait/One';
 export class Screens {
-
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    One_PhonePortrait
   ];
-
   static mapping = {
+    'One': {
+      PhonePortrait: One_PhonePortrait
+    }
   }
-
 }
-
-
