beede106c04616c2804a6546c9870e2cb1c6acc8
test <test@test.com>
2017-10-25 16:36:03 +0300

    [Studio] Delete screen "111"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,15 +1,9 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
-import { 111 _PhonePortrait } from '../pages/111/PhonePortrait/111';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent,
-    111 _PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    '111': {
-      PhonePortrait: 111 _PhonePortrait
-    }
-  }
+  static mapping = {}
 }
