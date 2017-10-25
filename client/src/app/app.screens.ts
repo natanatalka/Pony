c0346bc321f2cf89b2f553c4771b7dbe22749175
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 16:34:50 +0300

    [Studio] Add screen "First"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
+import { First_PhonePortrait } from '../pages/First/PhonePortrait/First';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    First_PhonePortrait
   ];
-  static mapping = {}
+  static mapping = {
+    'First': {
+      PhonePortrait: First_PhonePortrait
+    }
+  }
 }
