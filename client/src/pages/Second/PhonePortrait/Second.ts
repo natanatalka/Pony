e1805c7d18b58d1e21a078d1056172a6893ce469
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 16:34:23 +0300

    [Studio] Delete screen "Second"

--- client/src/pages/Second/PhonePortrait/Second.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { Screen } from 'app/screen';
-
-@Component({
-  selector: 'screen-second-phoneportrait',
-  templateUrl: 'Second.html'
-})
-export class Second_PhonePortrait extends Screen {
-}
