42819430438c438493c9a5e0c14151585c192fb1
test <test@test.com>
2017-10-25 16:35:52 +0300

    [Studio] Add screen "111"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
+import { 111 _PhonePortrait } from '../pages/111/PhonePortrait/111';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    111 _PhonePortrait
   ];
-  static mapping = {}
+  static mapping = {
+    '111': {
+      PhonePortrait: 111 _PhonePortrait
+    }
+  }
 }
