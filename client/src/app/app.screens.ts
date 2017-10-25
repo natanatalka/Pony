0128b56d0325ea466e099da98fa3e5f1443b9346
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 16:34:08 +0300

    [Studio] Add screen "Second"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,9 +1,15 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
+import { Second_PhonePortrait } from '../pages/Second/PhonePortrait/Second';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent
+    MenuComponent,
+    Second_PhonePortrait
   ];
-  static mapping = {}
+  static mapping = {
+    'Second': {
+      PhonePortrait: Second_PhonePortrait
+    }
+  }
 }
