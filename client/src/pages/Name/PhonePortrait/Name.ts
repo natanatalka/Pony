3b98a00cee37a3656957fe4f449e8321edaa6110
test <test@test.com>
2017-10-25 16:37:30 +0300

    [Studio] Add screen "Name"

---
+++ client/src/pages/Name/PhonePortrait/Name.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { Screen } from 'app/screen';
+
+@Component({
+  selector: 'screen-name-phoneportrait',
+  templateUrl: 'Name.html'
+})
+export class Name_PhonePortrait extends Screen {
+}
