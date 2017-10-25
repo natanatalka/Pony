af947f57f32cb9aebc98f7f9d2bcbf02928fb67c
PowWow Admin <admin@studio.dev.powwowinc.net>
2017-10-25 16:35:29 +0300

    [Studio] Delete screen "Second"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,15 +1,9 @@
 /***  Generated file, do not change.  */
 import { Page } from 'ionic-angular/navigation/nav-util';
 import { MenuComponent } from './menu/menu.component';
-import { Second_PhonePortrait } from '../pages/Second/PhonePortrait/Second';
 export class Screens {
   static declarations: Page[] = [
-    MenuComponent,
-    Second_PhonePortrait
+    MenuComponent
   ];
-  static mapping = {
-    'Second': {
-      PhonePortrait: Second_PhonePortrait
-    }
-  }
+  static mapping = {}
 }
